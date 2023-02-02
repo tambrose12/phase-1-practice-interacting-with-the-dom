bda268f4aabceb706e998df7d78bc6e84ca6c497
Taylor Ambrose <taykesby@gmail.com>
2023-02-02 11:29:30 -0500

    Tried to add function

--- js/challenge.js
+++ js/challenge.js
@@ -1,12 +1,21 @@
 
 //Feel free to change, assinging the minus button to variable named minus
-
 const minus = document.querySelector("#minus");
 
 //I just assigned the plus bottom
-
 const plus = document.querySelector("#plus");
 
 //assigned heartBtn variable to grab the heart id button for changes
-
 const heartBtn = document.getElementById("heart");
+
+//assigning counter variable
+const counter = document.getElementById('counter').textContent;
+
+
+const addToCounter = (count) => {
+    counter += 1
+    console.log(` ${count} `)
+
+}
+
+plus.addEventListener('click', addToCounter)
