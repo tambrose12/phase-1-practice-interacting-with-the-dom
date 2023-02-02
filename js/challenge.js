30210290904dba8a5c62c72b69bdf96a66f6706c
omar <omar.nabhan1958@gmail.com>
2023-02-02 10:41:23 -0500

    I just assigned the plus botton

--- js/challenge.js
+++ js/challenge.js
@@ -2,3 +2,7 @@
 //Feel free to change, assinging the minus button to variable named minus
 
 const minus = document.querySelector("#minus");
+
+const plus = document.querySelector("#plus");
+
+//I just assigned the plus bottom
