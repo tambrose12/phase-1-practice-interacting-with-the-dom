17ce48414e24180a5a4b1ff336e33aac0cdcf846
Taylor Ambrose <taykesby@gmail.com>
2023-02-03 11:39:46 -0500

    Finished pauseBtn. Set counter interval.

--- js/challenge.js
+++ js/challenge.js
@@ -1,36 +1,47 @@
 
-//Feel free to change, assinging the minus button to variable named minus
 const minusBtn = document.querySelector("#minus");
-//I just assigned the plus bottom
 const plusBtn = document.querySelector("#plus");
-//assigned heartBtn variable to grab the heart id button for changes
 const heartBtn = document.getElementById("heart");
-//assigning counter variable
 let counter = document.getElementById('counter');
-
 const likesContainer = document.querySelector('.likes')
+const formElement = document.getElementById('comment-form')
+const commentContainer = document.getElementById('list')
+const pauseBtn = document.getElementById('pause')
+const submitBtn = document.getElementById('submit')
 
-console.log(likesContainer)
+
+//Event Listeners calling functions:
+plusBtn.addEventListener('click', addToCounter)
+minusBtn.addEventListener('click', subtractFromCounter)
+heartBtn.addEventListener('click', addLike)
+formElement.addEventListener('submit', addComment)
+pauseBtn.addEventListener('click', togglePause)
+
+
+let paused = false
+
+let intervalID = setInterval(addToCounter, 1000)
 
 
 //Functions:
-const addToCounter = () => {
+function addToCounter() {
     const stringAsNumber = parseInt(counter.textContent)
     counter.textContent = stringAsNumber + 1
 }
 
-const subtractFromCounter = () => {
+function subtractFromCounter() {
     const stringAsNumber = parseInt(counter.textContent)
     counter.textContent = stringAsNumber - 1
 }
 
-const addLike = () => {
+function addLike() {
     const ourNumber = counter.innerText
     const foundLi = document.getElementById(`num-${ourNumber}`)
 
     if (foundLi) {
         const likesContainer = foundLi.querySelector('span')
         ++likesContainer.textContent
+
         if (likesContainer.textContent == '2') {
             foundLi.append('s')
         }
@@ -45,13 +56,29 @@ const addLike = () => {
     }
 }
 
-//Event Listeners calling functions:
-plusBtn.addEventListener('click', addToCounter)
 
-minusBtn.addEventListener('click', subtractFromCounter)
+function addComment(e) {
+    e.preventDefault()
+    const p = document.createElement('p')
+    const pText = e.target.comment.value
+    p.textContent = pText
+    commentContainer.append(p)
+}
 
-heartBtn.addEventListener('click', addLike)
+function togglePause(e) {
+    paused = !paused
+    e.target.textContent = paused ? 'resume' : 'pause'
+    const buttons = [
+        plusBtn, minusBtn, heartBtn, submitBtn
+    ]
+    buttons.forEach(button => button.disabled = paused ? true : false)
 
+    if (paused === true) {
+        clearInterval(intervalID)
+    } else {
+        setInterval(addToCounter, 1000)
+    }
+}
 
 
 
